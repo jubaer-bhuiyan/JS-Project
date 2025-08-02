1eac796668a560669a8f6473de9f3c76e0150d27
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-08-02 22:09:47 +0600

    Array Questions

--- command_line_app/index.js
+++ command_line_app/index.js
@@ -9,6 +9,21 @@ const gameState = {
     players: [],
 }
 
+const questions = [
+    {
+        prompt: "In which year was the first Star Wars movie released?",
+        answer: 1977,
+    },
+    {
+        prompt: "At the time of release, Titanic (1997) was the highest-grossing film of all time. How much did it gross worldwide in USD?",
+        answer: 2187463944,
+    },
+    {
+        prompt: "How many people did Arnold Schwarzenegger kill on screen in the Terminator franchise? (Data by jabhuiyan.com",
+        answer: 895,
+    },
+];
+
 const addPlayer = (player) => {
     gameState.players.push(player);
 }
