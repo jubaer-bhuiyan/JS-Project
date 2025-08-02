20546b950e35e4a1cd50f3b43196ef162ab592c5
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-08-02 20:55:13 +0600

    objects

--- command_line_app/index.js
+++ command_line_app/index.js
@@ -3,11 +3,19 @@ console.log("This is my First JS Project");
 const playerOne = "Jubaer";
 const playerTwo = "Junaed";
 
+const gameState = {
+    gameName: "Our amazing guessing game",
+    copyrightYear: 2025,
+}
+
 console.log("Hello, welcome to our game:", playerOne);
 console.log("Hello, welcome to our game:", playerTwo);
 
 const welcomePlayer = (player) => {
     console.log(`Welcome to our game, ${player}!`);
 }
+
+console.log(`Game Name: ${gameState.gameName}`);
+console.log(`Copyright Year: ${gameState.copyrightYear}`);
 
 welcomePlayer(playerOne);
