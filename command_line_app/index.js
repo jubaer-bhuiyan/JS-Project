a1d7f5fb706e96fe14c65c43dddf088bc2bd5846
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-08-02 20:46:40 +0600

    Function

--- command_line_app/index.js
+++ command_line_app/index.js
@@ -5,3 +5,9 @@ const playerTwo = "Junaed";
 
 console.log("Hello, welcome to our game:", playerOne);
 console.log("Hello, welcome to our game:", playerTwo);
+
+const welcomePlayer = (player) => {
+    console.log(`Welcome to our game, ${player}!`);
+}
+
+welcomePlayer(playerOne);
