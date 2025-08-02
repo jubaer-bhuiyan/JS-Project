563d15740e6974da9dbb0d80d36e91913ed48e13
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-08-02 21:18:26 +0600

    Array Push

--- command_line_app/index.js
+++ command_line_app/index.js
@@ -2,14 +2,24 @@ console.log("This is my First JS Project");
 
 const playerOne = "Jubaer";
 const playerTwo = "Junaed";
+//Exercise!!
+
+
 
 const gameState = {
     gameName: "Our amazing guessing game",
     copyrightYear: 2025,
+    players: [],
 }
 
-console.log("Hello, welcome to our game:", playerOne);
-console.log("Hello, welcome to our game:", playerTwo);
+const addPlayer = (player) => {
+    gameState.players.push(player);
+}
+
+console.log("Current players in the game:", gameState.players);
+addPlayer(playerOne);
+addPlayer(playerTwo);
+console.log("Players after adding in the game:", gameState.players);
 
 const welcomePlayer = (player) => {
     console.log(`Welcome to our game, ${player}!`);
---
+++ exploring/arrayPush.js
@@ -0,0 +1,9 @@
+const players = []
+
+console.log("Players: ", players)
+
+players.push(1, 2, 3, 4)
+const returnValueOfPush = players.push(5, 6, 7, 8)
+
+console.log(players)
+console.log("Return value of push:", returnValueOfPush)
