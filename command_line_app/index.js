f9d2c46e812d0e535be40ed097c1e419eaae3016
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-08-02 21:54:04 +0600

    Array Pop

--- command_line_app/index.js
+++ command_line_app/index.js
@@ -2,9 +2,6 @@ console.log("This is my First JS Project");
 
 const playerOne = "Jubaer";
 const playerTwo = "Junaed";
-//Exercise!!
-
-
 
 const gameState = {
     gameName: "Our amazing guessing game",
@@ -16,16 +13,26 @@ const addPlayer = (player) => {
     gameState.players.push(player);
 }
 
+const removeLatestAddedPlayer = () => {
+    return gameState.players.pop();
+}
+
 console.log("Current players in the game:", gameState.players);
 addPlayer(playerOne);
 addPlayer(playerTwo);
+addPlayer("The person with bad internet");
 console.log("Players after adding in the game:", gameState.players);
+
+const removedPlayer = removeLatestAddedPlayer();
+console.log("Players after removing the latest added player:", gameState.players);
+console.log("Removed player:", removedPlayer);
 
 const welcomePlayer = (player) => {
     console.log(`Welcome to our game, ${player}!`);
 }
 
+welcomePlayer(playerOne);
+welcomePlayer(playerTwo);
+
 console.log(`Game Name: ${gameState.gameName}`);
 console.log(`Copyright Year: ${gameState.copyrightYear}`);
-
-welcomePlayer(playerOne);
---
+++ exploring/arrayPop.js
@@ -0,0 +1,11 @@
+const players = []
+
+players.push("Alice", "Bob", "Charlie", "Diana")
+players.push("The player with bad luck")
+
+console.log(players)
+
+const returnValueOfPop = players.pop()
+console.log(returnValueOfPop)
+
+console.log(players)
