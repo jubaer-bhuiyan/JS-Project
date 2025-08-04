13e3d33ff6a482de28e5f44b5b1a825a0f636773
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-08-04 22:02:53 +0600

    Import and Export js

---
+++ exploring/default_export.js
@@ -0,0 +1,5 @@
+// Default export: name doesn't metter
+
+const aVariabeToExport = 42
+
+module.exports = aVariabeToExport;
---
+++ exploring/import.js
@@ -0,0 +1,5 @@
+// Default import
+
+const somethingImported = require("./default_export.js")
+
+console.log(somethingImported);
