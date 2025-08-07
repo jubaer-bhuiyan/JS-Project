dd9c0c62eeba2450be10ac2241fc7dc99833e761
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-08-07 22:13:50 +0600

    Making app user Interactive

--- command_line_app/index.js
+++ command_line_app/index.js
@@ -4,49 +4,67 @@ const { makeGuess } = require("./makeGuess.js");
 const { addPlayer, removeLatestAddedPlayer } = require("./players.js");
 const { gatherResults } = require("./gatherResults.js");
 const { caculateFinalResults } = require("./caculateFinalResults.js");
+const prompts = require('prompts');
 
-console.log("This is my First JS Project");
+const main = async () => {
+    console.log("This is my First JS Project");
 
-const playerOne = "Jubaer";
-const playerTwo = "Junaed";
+    const response = await prompts([
+        {
+            type: 'text',
+            name: 'playerOne',
+            message: 'What is your name, player one?',
+        },
+        {
+            type: 'text',
+            name: 'playerTwo',
+            message: 'What is your name, player two?',
+        },
+    ])
 
-console.log("Current players in the game:", gameState.players);
-addPlayer(playerOne);
-addPlayer(playerTwo);
-addPlayer("The person with bad internet");
-console.log("Players after adding in the game:", gameState.players);
+    const playerOne = response.playerOne;
+    const playerTwo = response.playerTwo;
 
-const removedPlayer = removeLatestAddedPlayer();
-console.log("Players after removing the latest added player:", gameState.players);
-console.log("Removed player:", removedPlayer);
+    console.log("Current players in the game:", gameState.players);
+    addPlayer(playerOne);
+    addPlayer(playerTwo);
+    addPlayer("The person with bad internet");
+    console.log("Players after adding in the game:", gameState.players);
 
-const welcomePlayer = (player) => {
-    console.log(`Welcome to our game, ${player}!`);
+    const removedPlayer = removeLatestAddedPlayer();
+    console.log("Players after removing the latest added player:", gameState.players);
+    console.log("Removed player:", removedPlayer);
+
+    const welcomePlayer = (player) => {
+        console.log(`Welcome to our game, ${player}!`);
+    }
+
+    console.log(`Game Name: ${gameState.gameName}`);
+    console.log(`Copyright Year: ${gameState.copyrightYear}`);
+
+    welcomePlayer(playerOne);
+    welcomePlayer(playerTwo);
+
+    startRound(0);
+    makeGuess(0, playerOne, 1969);
+    makeGuess(0, playerTwo, 1978);
+    const firstRoundResults = gatherResults(0);
+    console.log(JSON.stringify(firstRoundResults, undefined, 2));
+
+    startRound(1);
+    makeGuess(1, playerOne, 850);
+    makeGuess(1, playerTwo, 300);
+    const secondRoundResults = gatherResults(1);
+    console.log(JSON.stringify(secondRoundResults, undefined, 2));
+
+    startRound(2);
+    makeGuess(2, playerOne, 600);
+    makeGuess(2, playerTwo, 800);
+    const thirdRoundResults = gatherResults(2);
+    console.log(JSON.stringify(thirdRoundResults, undefined, 2));
+
+    const finalResults = caculateFinalResults();
+    console.log(finalResults);
 }
 
-console.log(`Game Name: ${gameState.gameName}`);
-console.log(`Copyright Year: ${gameState.copyrightYear}`);
-
-welcomePlayer(playerOne);
-welcomePlayer(playerTwo);
-
-startRound(0);
-makeGuess(0, playerOne, 1969);
-makeGuess(0, playerTwo, 1978);
-const firstRoundResults = gatherResults(0);
-console.log(JSON.stringify(firstRoundResults, undefined, 2));
-
-startRound(1);
-makeGuess(1, playerOne, 850);
-makeGuess(1, playerTwo, 300);
-const secondRoundResults = gatherResults(1);
-console.log(JSON.stringify(secondRoundResults, undefined, 2));
-
-startRound(2);
-makeGuess(2, playerOne, 600);
-makeGuess(2, playerTwo, 800);
-const thirdRoundResults = gatherResults(2);
-console.log(JSON.stringify(thirdRoundResults, undefined, 2));
-
-const finalResults = caculateFinalResults();
-console.log(finalResults);
+main();
--- exploring/prompts_with_await.js
+++ exploring/prompts_with_await.js
@@ -1,11 +1,18 @@
 const prompts = require('prompts');
 
 const askForUserName = async () => {
-    const response = await prompts({
-    type: 'text',
-    name: 'playerOne',
-    message: 'What is your name, player one?',
-});
+    const response = await prompts([
+        {
+            type: 'text',
+            name: 'playerOne',
+            message: 'What is your name, player one?',
+        },
+        {
+            type: 'text',
+            name: 'playerTwo',
+            message: 'What is your name, player two?',
+        },
+    ])
 
 console.log("response from prompts", response);
 }
