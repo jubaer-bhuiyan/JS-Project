e87587143d27f044176e54f659851df972cba3e5
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-08-07 12:43:45 +0600

    Calculate Final Result

--- command_line_app/caculateFinalResults.js
+++ command_line_app/caculateFinalResults.js
@@ -2,10 +2,27 @@ const { gameState } = require("./gameState.js");
 const { gatherResults } = require("./gatherResults.js");
 
 const caculateFinalResults = () => {
+    const results = {}
+
     gameState.rounds.forEach((round, index) => {
         const roundResults = gatherResults(index);
         console.log(roundResults, index);
+
+        const playerNames = Object.keys(roundResults)
+
+        //["Jubaer", "Junaed"]
+        playerNames.forEach((player) => {
+            // Jubaer OR Junaed
+            if (results[player] === undefined) {
+                results[player] = roundResults[player];
+            }
+            else {
+                results[player] += roundResults[player];
+            }
+        })
     })
+
+    return results;
 }
 
 module.exports = {
