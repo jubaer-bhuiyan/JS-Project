90c7d2bdc005465699f248ffe7a2199ee3bc1e7c
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-08-07 12:06:20 +0600

    Object.keys and Object.values

---
+++ exploring/object_keys.js
@@ -0,0 +1,10 @@
+const frenchEnglish = {
+    salut: "hi",
+    evident: "obvious",
+}
+
+const keysOfObject = Object.keys(frenchEnglish);
+console.log(keysOfObject);
+
+const valuesOfObject = Object.values(frenchEnglish);
+console.log(valuesOfObject);
