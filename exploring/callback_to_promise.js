ec249678bce3f86977ca5e6012f2c479f8f5a40a
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-08-08 10:19:39 +0600

    promise

--- exploring/callback_to_promise.js
+++ exploring/callback_to_promise.js
@@ -1,13 +1,20 @@
-setTimeout(() => {
-    console.log(3)
+//Functions are awaiteable when they return a Promise
 
-    setTimeout(() => {
-        console.log(2)
-
+const printNumberAfter1Second = (number) => {
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
-            console.log(1)
-        }, 1000);
+            console.log(number)
+            // reject(new Error("Something went wrong!"))
+            resolve("I am the return value")
+        }, 1000)
+    })
+}
 
-    }, 1000);
+const main = async () => {
+    const returnValue = await printNumberAfter1Second(3)
+    console.log(returnValue)
+    await printNumberAfter1Second(2)
+    await printNumberAfter1Second(1)
+}
 
-}, 1000);
+main()
