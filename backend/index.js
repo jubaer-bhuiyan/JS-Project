06b14e08c496c1b7b73083d65c5a0723395046ae
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-08-09 15:08:48 +0600

    Modern Import System (ESM)

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import { helloHandler } from "./handlers/hello.js"
 
 const app = express()
 
-app.get("/hello", helloHandler) 
+app.get("/hello", helloHandler)
 
 console.log("Launcing app on port 3331")
 app.listen(3331)
