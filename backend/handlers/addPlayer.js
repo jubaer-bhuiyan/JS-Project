cc819f214da69e5bf1eff7287e79ff072177d79d
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-08-11 20:48:42 +0600

    EndPoint of addPlayer

---
+++ backend/handlers/addPlayer.js
@@ -0,0 +1,4 @@
+export const addPlayerHandler = (request, response) => {
+    console.log("inside addPlayerHandler")
+    console.log("Request Body: ", request.body)
+}
--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,16 @@ import express from "express"
 import { helloHandler } from "./handlers/hello.js"
 import { stateHandler } from "./handlers/state.js"
 import { getPlayersHandler } from "./handlers/getPlayers.js"
+import { addPlayerHandler } from "./handlers/addPlayer.js"
 
 const app = express()
+
+app.use(express.json()) //passing JSON on Request
 
 app.get("/hello", helloHandler)
 app.get("/state", stateHandler)
 app.get("/players", getPlayersHandler)
+app.post("/players", addPlayerHandler)
 
 console.log("Launcing app on port 3331")
 app.listen(3331)
