0144bf48e6a945b0f95e817709377dbb39f51cfc
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-08-11 21:09:51 +0600

    Modify gameState from addPlayer endPoint

--- backend/handlers/addPlayer.js
+++ backend/handlers/addPlayer.js
@@ -1,4 +1,20 @@
+import { gameState } from "../gameState/index.js"
+
 export const addPlayerHandler = (request, response) => {
     console.log("inside addPlayerHandler")
     console.log("Request Body: ", request.body)
+
+    if (!request.body.name) {
+        response.status(400).send({
+            message: "You need to send a 'name' property"
+        })
+        return
+    }
+
+    gameState.players.push({
+        name: request.body.name,
+        points: 0,
+    })
+
+    response.status(201).send(gameState.players)
 }
